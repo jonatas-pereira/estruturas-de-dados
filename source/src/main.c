d368485cffe03ae4d43c86d89a4a37df12de3008
Jônatas Pereira <77009157+jonatas-pereira@users.noreply.github.com>
2024-07-22 15:47:54 -0300

    Update main.c

--- source/src/main.c
+++ source/src/main.c
@@ -1,30 +1,8 @@
 #include <stdio.h>
-#include <string.h>
 #include <stdlib.h>
-#include <stdbool.h>
-
-typedef struct p {
-  char nome[50];
-} Pessoa;
-
-void entradados(Pessoa *p) {
-  printf("Digite seu nome: ");
-  if(fgets(p->nome, sizeof(p->nome), stdin) != NULL) {
-    size_t len = strlen(p->nome);
-    if(len > 0 && p->nome[len-1] == '\n') {
-      p->nome[len-1] = '\0';
-    }
-  } else {
-    printf("Erro ao ler nome.\n");
-  }
-}
+#include "../include/variaveis-e-memoria.h"
 
 int main() {
-
-  Pessoa p;
-  entradados(&p);
-
-  printf("\nSeja bem-vindo, %s!\n", p.nome);
-
+  
   return 0;
 }
